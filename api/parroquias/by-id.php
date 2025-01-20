929fd6590f42aa0f99df9ffdf6aa0e1e1b56e568
fadrian06 <franyeradriansanchez@gmail.com>
2025-01-20 01:15:57 -0400

    fix client register

--- api/parroquias/by-id.php
+++ api/parroquias/by-id.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../db.php';
+require_once __DIR__ . '/../../functions.php';
 
 $stmt = db()->prepare('SELECT * FROM parroquias WHERE id = ?');
 $stmt->execute([$_GET['id']]);
